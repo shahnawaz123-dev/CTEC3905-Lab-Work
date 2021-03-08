0de03457b440a49ad16e3e830fdd2d37adab5eb3
shahnawaz123-dev <60345620+shahnawaz123-dev@users.noreply.github.com>
2021-03-08 13:21:58 +0000

    Adjustments made to menu

--- Lab4/scripts.js
+++ Lab4/scripts.js
@@ -1,4 +1,5 @@
 console.log("testing")
 menuToggler.addEventListener('click', ev => {
   Menu.classList.toggle('open');
+  menuToggler.textContent = menuToggler.textContent === "X" ? "≡" : "X";
 });
