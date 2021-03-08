6110b8773fcae844cbbb571a46658fcde17ce457
shahnawaz123-dev <60345620+shahnawaz123-dev@users.noreply.github.com>
2021-03-08 16:56:27 +0000

    Lab 6 halfway done, will complete later this week

---
+++ Lab6/todo.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function addItem(text, done) {
+  const item = document.createElement('li');
+  const button = document.createElement('button');
+button.textContent = "×";
+button.addEventListener('click', ev => {
+  item.remove();
+});
+item.appendChild(button);
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  label.textContent = text;
+  input.type = "checkbox";
+  input.checked = done;
+  input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+  label.htmlFor = input.id;
+  item.appendChild(label);
+  item.appendChild(input);
+  todo.appendChild(item);
+}
+
+
+add.addEventListener('click', ev => {
+  addItem(text.value);
+});
