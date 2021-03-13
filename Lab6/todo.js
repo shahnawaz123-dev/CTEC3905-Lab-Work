7d1d4efd3fdeeb99819517aff47b96c3a910d545
shahnawaz123-dev <60345620+shahnawaz123-dev@users.noreply.github.com>
2021-03-13 19:22:47 +0000

    Lab 6 completed

--- Lab6/todo.js
+++ Lab6/todo.js
@@ -1,11 +1,13 @@
 "use strict";
 
+(() => {
 function addItem(text, done) {
   const item = document.createElement('li');
   const button = document.createElement('button');
 button.textContent = "×";
 button.addEventListener('click', ev => {
   item.remove();
+  saveToStorage();
 });
 item.appendChild(button);
   const label = document.createElement('label');
@@ -14,6 +16,9 @@ item.appendChild(button);
   input.type = "checkbox";
   input.checked = done;
   input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+  input.addEventListener('input', ev => {
+  saveToStorage();
+});
   label.htmlFor = input.id;
   item.appendChild(label);
   item.appendChild(input);
@@ -22,5 +27,60 @@ item.appendChild(button);
 
 
 add.addEventListener('click', ev => {
-  addItem(text.value);
+  if(text.value) {
+    addItem(text.value);
+    text.value=null;
+    text.focus();
+    saveToStorage();
+  }
 });
+
+text.addEventListener('keydown', ev => {
+  if(ev.key == "Enter") {
+    add.click();
+  }
+});
+
+function clearList() {
+  while(todo.firstChild) {
+    todo.removeChild(todo.firstChild);
+  }
+}
+
+clear.addEventListener('click', ev => {
+  if(confirm("Are you sure you want to delete the entire list?"))
+  clearList();
+  saveToStorage();
+});
+
+[
+  {text: "Study learning materials", done: true},
+  {text: "Complete lab exercises", done: false},
+  {text: "Attend lab sessions", done: false},
+  {text: "Conduct self-directed research", done: false},
+  {text: "Ask questions", done: false}
+]
+
+function saveToStorage() {
+	const elements = Array.from(todo.querySelectorAll('li'));
+	const data = elements.map(el => {
+		 return {
+       text: el.querySelector('label').textContent,
+			 done: el.querySelector('input').checked
+		 }
+	});
+	localStorage.setItem(todo.id, JSON.stringify(data));
+}
+
+function loadFromStorage() {
+	const data = JSON.parse(localStorage.getItem(todo.id));
+  if(data) {
+    clearList();
+    for (const item of data) {
+  		addItem(item.text, item.done);
+  	}
+  }
+}
+
+loadFromStorage();
+})()
